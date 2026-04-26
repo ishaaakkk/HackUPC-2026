355751efcb948ae1b67ae699afd9f1bf10deab69
e-d-u1 <eduard.corrons@estudiantat.upc.edu>
2026-04-26 04:23:15 +0200

    Updated req

--- main.py
+++ main.py
@@ -27,4 +27,4 @@ async def read_root(request: Request):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=7999)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
